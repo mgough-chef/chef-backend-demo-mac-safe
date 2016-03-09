a62679d5a11b3261f0878897aea803a483dd13be
Charles Johnson <charles@opscode.com>
2016-03-08 16:45:05 -0800

    Updates to provisioning scripts
    Updates to provisioning scripts to catch changes in latest version of backend.

--- scripts/provision_be1.sh
+++ scripts/provision_be1.sh
@@ -5,15 +5,16 @@ dpkg -s chef-backend || dpkg -i /vagrant/chef-backend*
 mkdir -p /etc/chef-backend
 
 cat > /etc/chef-backend/chef-backend.rb <<EOF
-bootstrap_node true
 peers ['192.168.33.216','192.168.33.217']
 publish_address '192.168.33.215'
+vip "192.168.33.220"
+vip_interface "eth1"
 EOF
 
-chef-backend-ctl reconfigure
+chef-backend-ctl bootstrap
 
 [[ -f /vagrant/secrets.json ]] && rm /vagrant/secrets.json
-cp /etc/chef-backend/secrets.json /vagrant/gen_config
+cp /etc/chef-backend/secrets.json /vagrant/secrets.json
 
 [[ -f /vagrant/chef-server.rb.fe1 ]] && rm /vagrant/chef-server.rb.fe1
 chef-backend-ctl gen-server-config fe1.chef-demo.com > /vagrant/chef-server.rb.fe1
--- scripts/provision_be2.sh
+++ scripts/provision_be2.sh
@@ -4,12 +4,14 @@ dpkg -s chef-backend || dpkg -i /vagrant/chef-backend*
 
 mkdir -p /etc/chef-backend
 
-
 cat > /etc/chef-backend/chef-backend.rb <<EOF
 peers ['192.168.33.215', '192.168.33.217']
 publish_address '192.168.33.216'
+vip "192.168.33.220"
+vip_interface "eth1"
 EOF
 
+[[ -f /etc/chef-backend/secrets.json ]] || \
 cp /vagrant/secrets.json /etc/chef-backend/secrets.json
 
 chef-backend-ctl reconfigure
