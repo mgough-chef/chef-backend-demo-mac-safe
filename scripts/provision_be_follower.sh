f88c698b04f37cc6f7b2e1298cb8b3ab7e74418a
Sean Horn <furikake1+github@gmail.com>
2022-02-10 17:11:07 -0500

    Updates for modern followers
    * Show additional networks
    * Show additional package spec

--- scripts/provision_be_follower.sh
+++ scripts/provision_be_follower.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Get IP address of eth1 because this is a Vagrant box
-ipaddr=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
+# Get IP address of eth1 or enp0s8 because this is a Vagrant box
+#ipaddr=$(ifconfig eth1 | awk '/inet addr/{print substr($2,6)}')
+ipaddr=$(ifconfig enp0s8 | awk '/inet addr/{print substr($2,6)}')
 
 # Install the cluster package
-dpkg -s chef-backend &> /dev/null || dpkg -i /vagrant/chef-backend*.deb
+dpkg -s chef-backend &> /dev/null || dpkg -i /vagrant/chef-backend_2.0.30*.deb
 
 # Join the cluster
 chef-backend-ctl join-cluster 192.168.33.215 -p $ipaddr -s /vagrant/chef-backend-secrets.json --accept-license --yes
